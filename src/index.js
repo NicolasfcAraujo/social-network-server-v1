7d5beaa821f441eb73b6d768db74ac40b7f862bf
NICOLAS ARAUJO <nicolasfcaraujo@gmail.com>
2023-09-27 19:16:20 -0300

    origin changed again

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const app = express()
 const server = createServer(app)
 const io = new Server(server, {
   cors: {
-    origin: "https://lightref-social.vercel.app/"
+    origin: "https://lightref-social.vercel.app"
   }
 })
 
